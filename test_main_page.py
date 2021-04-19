2fba5b2362931af2210eb8f1e009d5e5a69062ca
GSilin <Eldest92@yandex.ru>
2021-04-19 19:48:24 +0300

    "добавил проверку наличия элементов на странице логина и регистрации"

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,14 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser,link)
     page.open()
     page.should_be_login_link()
+
+def test_elements_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_page()
